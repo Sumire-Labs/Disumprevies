b0c73af8e69f545d7aed62a4d311724a15c3081d
Hayato <hayato.fukami.0808@gmail.com>
2026-03-05 21:57:19 +0900

    Add logger module with setup function for initializing logging cogs

---
+++ src/cogs/logger/__init__.py
@@ -0,0 +1,16 @@
+from discord.ext import commands
+
+from .handlers import (
+    MessageHandler,
+    MemberHandler,
+    ServerHandler,
+    VoiceHandler,
+)
+
+
+async def setup(bot: commands.Bot) -> None:
+    """loggerモジュールのセットアップ"""
+    await bot.add_cog(MessageHandler(bot))
+    await bot.add_cog(MemberHandler(bot))
+    await bot.add_cog(ServerHandler(bot))
+    await bot.add_cog(VoiceHandler(bot))
